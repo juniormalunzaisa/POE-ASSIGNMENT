31f427506a572aaf48633cbc1438a67cc6d47e8c
juniormalunzaisa <ST10478297@rcconnect.edu.za>
2025-05-20 14:21:47 +0200

    Update Chatapp.java

--- PRLD assinment/chatapp/Chatapp.java
+++ PRLD assinment/chatapp/Chatapp.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
@@ -7,10 +7,30 @@ package com.mycompany.chatapp;
 /**
  *
  * @author RC_Student_lab
- */
-public class Chatapp {
+ */ package com.mycompany.chatapp;
+
+import javax.swing.JOptionPane;
+import org.json.JSONObject;
+
+public class Chatapp{
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+        Login user = new Login();
+
+        // === PART 1: Registration ===
+        String firstName = JOptionPane.showInputDialog("Enter your First Name:");
+        String lastName = JOptionPane.showInputDialog("Enter your Last Name:");
+        String username = JOptionPane.showInputDialog("Enter a Username (max 5 characters, must contain '_'):");
+        String password = JOptionPane.showInputDialog("Enter a Password (min 8 chars, with uppercase, number, and special character):");
+        String cellNumber = JOptionPane.showInputDialog("Enter Cell Number (e.g., +27831234567):");
+
+        user.setFirstname(firstName);
+        user.setLastName(lastName);
+
+        String registerMessage = user.registerUser(username, password, cellNumber);
+        JOptionPane.showMessageDialog(null, registerMessage);
+
+        if (!registerMessage.equals("User registered successfully.")) {
+            JOptionPane.showMessageDialog(null, "Registration failed. Exiting...");
+            return;
+        }
